722077dc72e3f90c1706e3f66d14a5dae8af3624
Seong Hoon Wee <mrgravity817@gmail.com>
2021-05-24 21:00:10 +0900

    Replace read/write fragment to modify

--- src/hooks/useToggleLike.tsx
+++ src/hooks/useToggleLike.tsx
@@ -15,27 +15,14 @@ export const useToggleLike = (id: number, isLiked: boolean, likes: number) => {
     update: (cache, result) => {
       const { ok } = result.data?.toggleLike!;
       if (ok) {
-        const fragmentId = `Photo:${id}`;
-        const fragment = gql`
-          fragment PhotoLike on Photo {
-            isLiked
-            likes
-          }
-        `;
-        const result = cache.readFragment<ToggleLike>({
-          id: fragmentId,
-          fragment,
+        const photoId = `Photo:${id}`;
+        cache.modify({
+          id: photoId,
+          fields: {
+            isLiked: (prev) => !prev,
+            likes: (prev) => (isLiked ? prev - 1 : prev + 1),
+          },
         });
-        if (result && "isLiked" in result && "likes" in result) {
-          cache.writeFragment({
-            id: fragmentId,
-            fragment,
-            data: {
-              isLiked: !isLiked,
-              likes: isLiked ? likes - 1 : likes + 1,
-            },
-          });
-        }
       }
     },
   });
